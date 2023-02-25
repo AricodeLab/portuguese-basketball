2e4d1334d13aed0046e6096ef6ca8657c4e896c8
higordevv <pedrohigor2004@gmail.com>
2023-02-25 20:53:48 -0300

    Mudanças a serem submetidas: 	renamed:    src/renderer/src/contexts /AppContext.tsx -> src/renderer/src/contexts/AppContext.tsx 	renamed:    src/renderer/src/contexts /theme_context /ThemeContext.tsx -> src/renderer/src/contexts/theme_context /ThemeContext.tsx 	modified:   src/renderer/src/main.tsx

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import App from './App'
-import { AppContextProvider } from './contexts /AppContex'
+import { AppContextProvider } from './contexts/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
