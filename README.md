2f405c7985a6be1e773bb743a138ff0c47eea83e
higordevv <pedrohigor2004@gmail.com>
2023-02-24 23:49:22 -0300

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # portuguese-basketball
+
 Projeto basquete na europa e muitos euro
+
+## Links para Build do exe:
+
+- https://www.electron.build/multi-platform-build#to-build-app-for-windows-on-linux
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-slate-800 flex justify-center items-center h-screen w-screen">
+        <h1 className="text-6xl text-white">Funcionando</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
