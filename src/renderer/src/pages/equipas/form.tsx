132e92f619507c6009a70fdf990ad8038ce341a6
joao calado <jcaladomoura@gmail.com>
2023-03-02 23:46:04 -0300

    img

--- src/renderer/src/pages/equipas/form.tsx
+++ src/renderer/src/pages/equipas/form.tsx
@@ -1,7 +1,29 @@
 import { Button } from '../../Components/'
 import { Div, Header } from './style'
+import { useForm } from 'react-hook-form'
+import useFilePreview from '@renderer/hooks/useFilePreview'
+import { useEffect, useState } from 'react'
 
+interface IFormInputs {
+  file: File
+}
 function Form(): JSX.Element {
+  const { register, watch } = useForm<IFormInputs>()
+
+  const file = watch('file')
+  const [filepreviw, setFilepreviw] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (file && file[0]) {
+
+      const reader = new FileReader();
+
+      const newlink =URL.createObjectURL(file[0]);
+      setFilepreviw(newlink)
+    }
+    console.log(filepreviw)
+  }, [file]);
+
   return (
     <Div>
       <Header>
@@ -9,8 +31,10 @@ function Form(): JSX.Element {
         <Button type={3}>Importar</Button>
       </Header>
       <div>
-        <h1>logo</h1>
-        
+        <h1>Logo</h1>
+        <input type="file" id="file" {...register('file')} />
+
+        {filepreviw ? <img src={filepreviw} alt="preview" /> : null}
       </div>
     </Div>
   )
